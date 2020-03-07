2ac438e73da508049065aa89f4755c2e79073e9a
Ablo224 <ablobalde92@gmail.com>
2020-03-07 01:29:59 +0100

    init du projet pglp_3.2

---
+++ src/main/java/ufruvsq21606235/App.java
@@ -0,0 +1,13 @@
+package ufruvsq21606235;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
